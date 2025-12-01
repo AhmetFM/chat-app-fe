16f48e41e87bc4a3031db390fcfe98e0f0529c28
Ahmet Furkan Meric <afurkanmeric@gmail.com>
2025-12-01 16:49:44 +0300

    Some little changes on tab bar and SingleChatRow

--- app/(home)/_layout.tsx
+++ app/(home)/_layout.tsx
@@ -1,22 +1,50 @@
-import { Ionicons } from "@expo/vector-icons";
-import { Tabs } from "expo-router";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { Tabs, useSegments } from "expo-router";
 import React from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const HomeLayout = () => {
+  const segments = useSegments();
+
   return (
-    <Tabs>
-      <Tabs.Screen name="updates" />
-      <Tabs.Screen
-        name="chats"
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="chatbubbles" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tabs.Screen name="settings" />
-    </Tabs>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Tabs>
+        <Tabs.Screen
+          name="updates"
+          options={{
+            headerShown: false,
+            title: "Updates",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="update" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="chats"
+          options={{
+            headerShown: false,
+            title: "Chats",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="chatbubbles" color={color} size={size} />
+            ),
+            tabBarStyle: {
+              backgroundColor: "#f5f5f5",
+              // display: segments[2] === "[id]" ? "none" : "flex",
+            },
+          }}
+        />
+        <Tabs.Screen
+          name="settings"
+          options={{
+            headerShown: false,
+            title: "Settings",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="settings" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tabs>
+    </GestureHandlerRootView>
   );
 };
 
--- components/SingleChatRow.tsx
+++ components/SingleChatRow.tsx
@@ -1,8 +1,9 @@
 import { format } from "date-fns";
 import React from "react";
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableHighlight, View } from "react-native";
 
 import noProfileImage from "@/assets/images/no-profile-image.svg";
+import { Link } from "expo-router";
 
 const no_profile_image = Image.resolveAssetSource(noProfileImage).uri;
 
@@ -26,33 +27,37 @@ const SingleChatRow = ({
   unreadCount,
 }: SingleChatRowProps) => {
   return (
-    <View className="w-full h-20 flex-row items-center gap-[14px] pl-5 py-[10px]">
-      {/* Profile Img */}
-      <Image
-        className="w-[50px] h-[50px] rounded-full"
-        source={{ uri: img ? img : no_profile_image }}
-      />
+    <Link href={`/(home)/chats/${id}`} asChild>
+      <TouchableHighlight activeOpacity={0.6} underlayColor="#f0f0f0">
+        <View className="w-full h-20 flex-row items-center gap-[14px] pl-5 py-[10px]">
+          {/* Profile Img */}
+          <Image
+            className="w-[50px] h-[50px] rounded-full"
+            source={{ uri: img ? img : no_profile_image }}
+          />
 
-      <View className="flex-1">
-        {/* Title, lastMessage and date. */}
-        <Text className="text-lg font-bold">{from}</Text>
-        <Text className="text-base text-gray-500">
-          {msg.length > 40 ? `${msg.substring(0, 40)}...` : msg}
-        </Text>
-      </View>
-      <View className="gap-2">
-        <Text
-          className={`text-gray-500 pr-5 items-start ${unreadCount && "text-green-600"} `}
-        >
-          {format(date, "MM.dd.yy")}
-        </Text>
-        {unreadCount >= 1 && (
-          <Text className="w-[18px] h-[18px] rounded-full self-end mr-4 text-center font-semibold  text-white bg-green-600">
-            {unreadCount}
-          </Text>
-        )}
-      </View>
-    </View>
+          <View className="flex-1">
+            {/* Title, lastMessage and date. */}
+            <Text className="text-lg font-bold">{from}</Text>
+            <Text className="text-base text-gray-500">
+              {msg.length > 40 ? `${msg.substring(0, 40)}...` : msg}
+            </Text>
+          </View>
+          <View className="gap-2">
+            <Text
+              className={`text-gray-500 pr-5 items-start ${unreadCount && "text-green-600"} `}
+            >
+              {format(date, "MM.dd.yy")}
+            </Text>
+            {unreadCount >= 1 && (
+              <Text className="w-[18px] h-[18px] rounded-full self-end mr-4 text-center font-semibold  text-white bg-green-600">
+                {unreadCount}
+              </Text>
+            )}
+          </View>
+        </View>
+      </TouchableHighlight>
+    </Link>
   );
 };
 
